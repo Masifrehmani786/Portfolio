b0bfa45abcf8b046929026e6986636b593e86d63
Muhammad Asif Bashir <masifrehmani.itroots@gmail.com>
2022-09-06 14:29:37 +0500

    Blog link changes

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+
+
+import chatify from "../../Assets/Projects/chatify.png";
+
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="Textutils"
+              description="Convert text , clear text , uppercase to lower and lower to upper by me in React js"
+              ghLink="https://github.com/Masifrehmani786/taxtutils"
+              
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="E-Commerce"
+              description="E-commerce web app  by me in React js"
+              ghLink="https://github.com/Masifrehmani786/Eccomerce-React"
+              
+            />
+          </Col>
+
+         
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
